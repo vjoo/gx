d20bfcd82c7213add1322c0b4ee1663efe74b1ac
vjoo <vjoo@163.com>
2023-04-16 13:28:00 +0800

    wwww

--- js/ajaxSet.js
+++
@@ -1,9 +0,0 @@
-var _MODE = 'dev';
-
-//数据接口地址
-var baseURL = {
-  dev: 'https://xxx',
-  //dev: 'https://api-prd.uidt.net/phanerozoic/bi/v1a',
-  prod: 'https://jjj',
-}[_MODE];
-
--- js/xialakuang.js
+++ js/xialakuang.js
@@ -275,4 +275,32 @@ $(function () {
       placeholder: 'box6',//默认提示
     }
   ); 
+  //启动后值班人员
+  var dataLeft11 = [
+    { name: '当前应急值班人员', id: 1 },
+    { name: '全部', id: 2 },
+    { name: 'xxxx', id: 3 },
+    { name: 'xxxx', id: 4 }
+  ],
+    fnBackLeft11 = function (result) {
+      console.log(result.name + ' ' + result.id);
+    };
+  new SelectBox($('#leftSelect11'), dataLeft11, fnBackLeft11,
+    {
+      color: '#fff',//输入框字体颜色
+      optionColor: '#fff',//下拉框字体颜色
+      arrowColor: '#fff',//箭头颜色
+      backgroundColor: '#0A1D56',//背景色颜色
+      borderColor: '#02A7F0',//边线颜色
+      hoverColor: '#02A7F0',//下拉框HOVER颜色
+      borderWidth: 1,//边线宽度
+      defalut: 'firstData',//默认显示内容。如果是'firstData',则默认显示第一个
+      arrowBorderWidth: 0,//箭头左侧分割线宽度。如果为0则不显示
+      borderRadius: 2,//边线圆角
+      height: 30,//高
+      width: 150,
+      textIndent: 5,
+      placeholder: 'box6',//默认提示
+    }
+  ); 
 });
