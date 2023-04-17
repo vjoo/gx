2eec4825a116d8ab851bab7031f81420c696a14d
vjoo <vjoo@163.com>
2023-04-17 10:20:11 +0800

    222

--- js/box3.js
+++ js/box3.js
@@ -2,130 +2,109 @@ var dom = document.getElementById("gh");
 var myChart = echarts.init(dom);
 var app = {};
 
-// var option;
+var option;
 
 /**
  * 图标所需数据
  */
-// var data = {
-//   id: "echartPieyyyy",
-//   value: [10, 22, 22,],
-//   legend: ["未响应", "处理中", "已完成"],
-//   color: ["#D9001B", "#F59A23", "#70B603"],
-//   // tooltipShow:false,    //设置悬浮提示显示              --默认显示true
-//   // hoverAnimation:false, //设置鼠标悬浮点击饼图动画效果  --默认开启动画true
-//   title: "",
-// };
-
+var data = {
+	id: "echartPiesss",
+	value: [10, 22, 22, ],
+	legend: ["未响应", "处理中", "已完成"],
+	textStyle: {
+		fontSize: 12, //字体大小
+		color: '#ffffff' //字体颜色
+	},
+	color: ["#D9001B", "#F59A23", "#70B603"],
+	// tooltipShow:false,    //设置悬浮提示显示              --默认显示true
+	// hoverAnimation:false, //设置鼠标悬浮点击饼图动画效果  --默认开启动画true
+	title: "",
+};
 
 ////////////////////////////////////////
 
-// var option = {
-//   title: {
-//     x: "2%",
-//     y: "2%",
-//     textStyle: {
-//       fontWeight: 400,
-//       fontSize: 14,
-//       color: "#ffffff",
-//     },
-//     text: data.title || "",
-//   },
-//   tooltip: {
-//     trigger: "item",
-//     show: data.tooltipShow === false ? false : true,
-//     //   formatter: "{b}: {c} ({d}%)"
-//   },
-//   legend: {
-//     orient: "horizontal",
-//     top: 10,
-//     icon: "circle",
-//     selectedMode: false,
-//     itemWidth: 6,
-//     itemHeight: 6,
-//     itemGap: 6,
-//     borderRadius: 6,
-//     data: data.legend,
-//   },
-//   series: [
-//     {
-//       type: "pie",
-//       top: '14%',
-//       // clickable:false,
-//       // selectedMode: 'single',//单点击设置
-//       hoverAnimation: data.hoverAnimation === false ? false : true,
-//       radius: ["40%", "90%"],
-//       color: data.color,
-//       label: {
-//         normal: {
-//           position: "inner",
-//           // formatter: '{d}%',
-//           formatter: function (param) {
-//             if (!param.percent) return "";
-//             var f = Math.round(param.percent * 10) / 10;
-//             var s = f.toString();
-//             var rs = s.indexOf(".");
-//             if (rs < 0) {
-//               rs = s.length;
-//               s += ".";
-//             }
-//             while (s.length <= rs + 1) {
-//               s += "0";
-//             }
-//             return s + "%";
-//           },
-//           textStyle: {
-//             color: "#ffffff",
-//             fontSize: 16,
-//           },
-//         },
-//       },
-//       labelLine: {
-//         normal: {
-//           show: false,
-//         },
-//       },
-//       data: seriesData,
-//     },
-//   ],
-// };
-option = {
-  tooltip: {
-      trigger: 'item',
-      formatter: "{a} <br/>{b} : {c} ({d}%)"
-  },
-  legend: {
-    top:'10%',
-    y: 'top',
-    data: ['未响应', '处理中', '已完成'],
-    textStyle: {
-      color: '#fff'
-    }
-  },
-  
-  series: [
-      {
-          name: '半径模式',
-          type: 'pie',
-          radius: [30, 68],
-          center: ['50%', '60%'],
-          color: ["#D9001B", "#F59A23", "#70B603"],
-          data: [
-              { value: 10, name: '未响应' },
-              { value: 22, name: '处理中' },
-              { value: 22, name: '已完成' },
-          ]
-      }
-  ]
+/**
+ * 数据处理
+ */
+var seriesData = [];
+data.value.forEach(function(item, index) {
+	seriesData.push({
+		value: item,
+		name: data.legend[index],
+	});
+});
+////////////////////////////////////////
+
+var option = {
+	title: {
+		x: "2%",
+		y: "2%",
+		textStyle: {
+			fontWeight: 400,
+			fontSize: 14,
+			color: "#ffffff",
+		},
+		text: data.title || "",
+	},
+	tooltip: {
+		trigger: "item",
+		show: data.tooltipShow === false ? false : true,
+		//   formatter: "{b}: {c} ({d}%)"
+	},
+	legend: {
+		orient: "horizontal",
+		top: 10,
+		icon: "circle",
+		selectedMode: false,
+		itemWidth: 6,
+		itemHeight: 6,
+		itemGap: 6,
+		borderRadius: 6,
+		data: data.legend,
+		textStyle:data.textStyle
+	},
+	series: [{
+		type: "pie",
+		top: '14%',
+		// clickable:false,
+		// selectedMode: 'single',//单点击设置
+		hoverAnimation: data.hoverAnimation === false ? false : true,
+		radius: ["40%", "90%"],
+		color: data.color,
+		label: {
+			normal: {
+				position: "inner",
+				// formatter: '{d}%',
+				formatter: function(param) {
+					if (!param.percent) return "";
+					var f = Math.round(param.percent * 10) / 10;
+					var s = f.toString();
+					var rs = s.indexOf(".");
+					if (rs < 0) {
+						rs = s.length;
+						s += ".";
+					}
+					while (s.length <= rs + 1) {
+						s += "0";
+					}
+					return s + "%";
+				},
+				textStyle: {
+					color: "#ffffff",
+					fontSize: 16,
+				},
+			},
+		},
+		labelLine: {
+			normal: {
+				show: false,
+			},
+		},
+		data: seriesData,
+	}, ],
 };
 
-  myChart.setOption(option);
 
-
-// $(function () {
-  $('#dian').click(function () {
-    $('#phone').show();
-    // $('#dialog1').css('visibility','visible')
-  })
-// })
-
+if (option && typeof option === 'object') {
+	myChart.setOption(option);
+}
--- js/box4.js
+++ js/box4.js
@@ -1,107 +1,120 @@
-var dom = document.getElementById("fss_gh");
-var myChart = echarts.init(dom);
-var app = {};
+let fssGhChart = null;
 
-var option;
+function fss_ghChart() {
+	var dom = document.getElementById("fss_gh");
+	if (fssGhChart == null) {
+		fssGhChart = echarts.init(dom);
+	}
+	else{
+		fssGhChart.clear();
+	}
+	fssGhChart.resize();
+	var app = {};
 
-/**
- * 图标所需数据
- */
- var data = {
-    id: "echartPieyyyy",
-    value: [10, 22, 22, 12,],
-    legend: ["未响应", "未处理","处理中" ,"已反馈"],
-    color: ["#D9001B", "#E16757","#F59A23","#70B603"],
-    // tooltipShow:false,    //设置悬浮提示显示              --默认显示true
-    // hoverAnimation:false, //设置鼠标悬浮点击饼图动画效果  --默认开启动画true
-    title: "",
-  };
-  
-  ////////////////////////////////////////
-  
-  /**
-   * 数据处理
-   */
-  var seriesData = [];
-  data.value.forEach(function (item, index) {
-    seriesData.push({
-      value: item,
-      name: data.legend[index],
-    });
-  });
-  ////////////////////////////////////////
-  
-  var option = { 
-    title: {
-      x: "2%",
-      y: "2%",
-      textStyle: {
-        fontWeight: 400,
-        fontSize: 14,
-        color: "#ffffff",
-      },
-      text: data.title || "",
-    },
-    tooltip: {
-      trigger: "item",
-      show: data.tooltipShow === false ? false : true,
-      //   formatter: "{b}: {c} ({d}%)"
-    },
-    legend: {
-      orient: "horizontal",
-      top: 10,
-      icon: "circle",
-      selectedMode: false,
-      itemWidth: 6,
-      itemHeight: 6,
-      itemGap: 6,
-      borderRadius: 6,
-      data: data.legend,
-    },
-    series: [
-      {
-        type: "pie",
-        top:'14%',
-        // clickable:false,
-        // selectedMode: 'single',//单点击设置
-        hoverAnimation: data.hoverAnimation === false ? false : true,
-        radius: ["40%", "90%"],
-        color: data.color,
-        label: {
-          normal: {
-            position: "inner",
-            // formatter: '{d}%',
-            formatter: function (param) {
-              if (!param.percent) return "";
-              var f = Math.round(param.percent * 10) / 10;
-              var s = f.toString();
-              var rs = s.indexOf(".");
-              if (rs < 0) {
-                rs = s.length;
-                s += ".";
-              }
-              while (s.length <= rs + 1) {
-                s += "0";
-              }
-              return s + "%";
-            },
-            textStyle: {
-              color: "#ffffff",
-              fontSize: 16,
-            },
-          },
-        },
-        labelLine: {
-          normal: {
-            show: false,
-          },
-        },
-        data: seriesData,
-      },
-    ],
-  };
-  
+	var option;
 
-if (option && typeof option === 'object') {
-    myChart.setOption(option);
+	/**
+	 * 图标所需数据
+	 */
+	var data = {
+		id: "echartPieyyyy",
+		value: [10, 22, 22, 12, ],
+		textStyle: {
+			fontSize: 12, //字体大小
+			color: '#ffffff' //字体颜色
+		},
+		legend: ["未响应", "未处理", "处理中", "已反馈"],
+		color: ["#D9001B", "#E16757", "#F59A23", "#70B603"],
+		// tooltipShow:false,    //设置悬浮提示显示              --默认显示true
+		// hoverAnimation:false, //设置鼠标悬浮点击饼图动画效果  --默认开启动画true
+		title: "",
+	};
+
+	////////////////////////////////////////
+
+	/**
+	 * 数据处理
+	 */
+	var seriesData = [];
+	data.value.forEach(function(item, index) {
+		seriesData.push({
+			value: item,
+			name: data.legend[index],
+		});
+	});
+	////////////////////////////////////////
+
+	var option = {
+		title: {
+			x: "2%",
+			y: "2%",
+			textStyle: {
+				fontWeight: 400,
+				fontSize: 14,
+				color: "#ffffff",
+			},
+			text: data.title || "",
+		},
+		tooltip: {
+			trigger: "item",
+			show: data.tooltipShow === false ? false : true,
+			//   formatter: "{b}: {c} ({d}%)"
+		},
+		legend: {
+			orient: "horizontal",
+			top: 10,
+			icon: "circle",
+			selectedMode: false,
+			itemWidth: 6,
+			itemHeight: 6,
+			itemGap: 6,
+			borderRadius: 6,
+			data: data.legend,
+			textStyle: data.textStyle
+		},
+		series: [{
+			type: "pie",
+			top: '14%',
+			// clickable:false,
+			// selectedMode: 'single',//单点击设置
+			hoverAnimation: data.hoverAnimation === false ? false : true,
+			radius: ["40%", "90%"],
+			color: data.color,
+			label: {
+				normal: {
+					position: "inner",
+					// formatter: '{d}%',
+					formatter: function(param) {
+						if (!param.percent) return "";
+						var f = Math.round(param.percent * 10) / 10;
+						var s = f.toString();
+						var rs = s.indexOf(".");
+						if (rs < 0) {
+							rs = s.length;
+							s += ".";
+						}
+						while (s.length <= rs + 1) {
+							s += "0";
+						}
+						return s + "%";
+					},
+					textStyle: {
+						color: "#ffffff",
+						fontSize: 16,
+					},
+				},
+			},
+			labelLine: {
+				normal: {
+					show: false,
+				},
+			},
+			data: seriesData,
+		}, ],
+	};
+
+
+	if (option && typeof option === 'object') {
+		fssGhChart.setOption(option);
+	}
 }
